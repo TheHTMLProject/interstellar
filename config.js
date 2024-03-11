c9d4817a32c48f0b8f286f022b672e6c93f701a2
The HTML Project <159310111+TheHTMLProject@users.noreply.github.com>
2024-03-11 09:41:48 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,12 @@
 const config = {
-  challenge: false, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
-    interstellar: 'password',
+    Asher: 'pwd2logon',
+     Ben: 'Dover',
+    cbaiata: 'helicopter',
+    geronimo: 'monkey',
+    
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
