14dd1f8c8244db6b50f1d2f744e9cf3d7dd3cb36
The HTML Project <159310111+TheHTMLProject@users.noreply.github.com>
2024-04-29 08:52:35 -0700

    Update config.js

--- config.js
+++ config.js
@@ -18,6 +18,7 @@ const config = {
     justin: 'hello',
     olivia: 'tejada',
     hudson: 'hhh',
+    scott: 'sss',
   },
   routes: true, // Change this to false if you just want to host a bare server.
   local: true, // Change this to false to disable local assets.
